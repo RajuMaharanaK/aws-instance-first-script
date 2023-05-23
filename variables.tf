787bb13b3c849ef8f24cb47703562ed6ccb8c20a
RajuMaharanaK <110522547+RajuMaharanaK@users.noreply.github.com>
2023-05-23 13:06:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,8 +6,8 @@ variable "ami_id" {
   type = map(string)
 
   default = {
-    us-east-1(ami-035b3c7efe6d061d5)
-    eu-west-2(ami-132b3c7efe6sdfdsfd)
-    eu-central-1(ami-9787h5h6nsn)
+    us-east-1    = "ami-035b3c7efe6d061d5"
+    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
+    eu-central-1 = "ami-9787h5h6nsn"
   }
 }
